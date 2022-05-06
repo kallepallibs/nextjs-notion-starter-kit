fa0de6711a06cb80a8637797f3c1edaab335fe7a
Kallepalli <87940334+kallepallibs@users.noreply.github.com>
2022-05-06 08:53:53 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,18 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'a8a189e4198e48eaaaa56eae35d83e8c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
+  
   rootNotionSpaceId: null,
 
-  // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
 
-  // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
-
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+ // basic site info (required)
+  name: 'Bushan LMS',
+  domain: 'ramananda.live',
+  author: 'Bushan K',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
